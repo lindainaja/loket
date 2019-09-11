6c9b86ec168288f68ea52922f40bd5cf284d022a
Putra <cristminix@gmail.com>
2019-09-11 17:54:30 +0700

    init adm loket

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -9,15 +9,15 @@ function parse_setting_template($text,$i=1)
 	foreach ($captured[1] as $index => $key) {
 		$real_value = $ci->m_setting->get_value($key);
 		if(!empty($real_value)){
-			$parsed = str_replace($captured[0], $real_value, $text);
+			$text = str_replace($captured[0], $real_value, $text);
+			if($i == 1){
+				$text = parse_setting_template($text,2);
+			}
 		}
 		else{
-
-			
+			// $parsed = $text;
 		}
-		if($i == 1){
-			$parsed = parse_setting_template($parsed,2);
-		}
+		
 	}
-	return $parsed;
+	return $text;
 }
---
+++ application/views/adm/loket.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Adm Loket</title>
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/assets/css/bootstrap-reboot.min.css">
+
+	<script type="text/javascript" src="<?=base_url()?>public/assets/js/jquery-3.2.1.slim.min.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>public/assets/js/popper.min.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>public/assets/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Adm Loket</h1>
+		</div>
+	</div>
+	<div class="row">
+
+		<div class="col-md-8">
+			
+		</div>
+		<div class="col-md-4">
+			<table class="table table-bordered">
+				<thead>
+					<tr><th>NOMOR ANTRIAN</th></tr>
+				</thead>
+				<tbody>
+					<tr><td><h1><span id="nomor_antrian">A0001</span></h1></td></tr>
+					<tr><td><span id="nama_loket">Loket 1</span></td></tr>
+					<tr><td><i class="fas fa-volume-up"></i> Call</td></tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="row">
+		
+		<div class="col-md-12">
+		
+		</div>
+
+	
+</div>
+</div>
+
+</body>
+</html>
